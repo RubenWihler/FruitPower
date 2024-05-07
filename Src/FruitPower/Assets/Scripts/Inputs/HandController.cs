2199403e877236a2b48a3e90e8c42a31d86ea72b
wihler <wihlerruben@gmail.com>
2024-05-07 08:00:33 +0200

    Removed HandController unused input

--- Src/FruitPower/Assets/Scripts/Inputs/HandController.cs
+++ Src/FruitPower/Assets/Scripts/Inputs/HandController.cs
@@ -16,12 +16,10 @@ namespace Inputs
     public sealed class HandController : MonoBehaviour
     {
         [Header("Input Actions")]
-        [SerializeField, Tooltip("Reference vers l'input action de pinch")]
-        private InputActionProperty pinchAction;
         [SerializeField, Tooltip("Reference vers l'input action de grip")]
         private InputActionProperty gripAction;
 
-        [Header("Animaiton")]
+        [Header("Animation")]
         [SerializeField, Tooltip("Reference vers l'animator de la main")]
         private Animator animator;
 
@@ -30,10 +28,6 @@ namespace Inputs
         /// </summary>
         private void Update()
         {
-            //pinch
-            var trigger_value = pinchAction.action.ReadValue<float>();
-            animator.SetFloat("Trigger", trigger_value);
-
             //grip
             var grip_value = gripAction.action.ReadValue<float>();
             animator.SetFloat("Grip", grip_value);
